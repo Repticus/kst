d4a05b262c250578348c59082a9a17942c815079
Repticus <martin.bubik@flashlink.cz>
2015-08-29 00:22:40 +0200

    set up setlocale to config.neon

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,14 +1,9 @@
 <?php
 
-setlocale(LC_ALL, "cs_CZ.UTF-8"); //unix
-//setlocale(LC_ALL, "czech"); //windows
-
 require __DIR__ . '/../vendor/autoload.php';
 
 $configurator = new Nette\Configurator;
 
-//$configurator->setDebugMode('23.75.345.200'); // enable for your remote IP
-//$configurator->setDebugMode(FALSE);
 $configurator->enableDebugger(__DIR__ . '/../log');
 
 $configurator->setTempDirectory(__DIR__ . '/../temp');
@@ -23,5 +18,7 @@ if (file_exists(__DIR__ . '/config.local.neon')) {
 }
 
 $container = $configurator->createContainer();
+
+setlocale(LC_ALL, $container->parameters['setlocale']);
 
 return $container;
